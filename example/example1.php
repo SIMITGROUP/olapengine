4b652b7eb5227323e048e7fcc991307367bfa842
Ks Tan <kstan@simitgroup.com>
2018-08-20 09:32:59 +0800

    improve example

--- example/example1.php
+++ example/example1.php
@@ -7,8 +7,6 @@ include 'examplearray.php';
 $dimensions=[
 	['field'=>'agent','type'=>'string'],
 	['field'=>'item','type'=>'string'],
-	// ['field'=>'country','type'=>'string','parent'=>'region'],
-	// ['field'=>'city','type'=>'string','parent'=>'country'],
 	['field'=>'region','type'=>'string',
 			'child'=>[
 				['field'=>'country','type'=>'string',
@@ -31,52 +29,21 @@ $measures=[
 $facts = $data;
 
 $olap = new OLAPEngine();
-// $column=['region','country','city'];
-$column=['region'];
-// $column=['region'];
-$filter=['region'=>['SEA']];
 
+//get region list without filter
+$column=['region']; 
+$filter=[];
+
+//get country list under region SEA
+//$column=['region','country']; 
+//$filter=['region'=>['SEA']];
+
+
+//get all country list
+//$column=['region','country']; 
+//$filter=['region'=>['*']];
 $cube = $olap->createCube($facts,$dimensions,$measures);
 $data=$olap->getDimensionValues($cube,$column,$filter,'facts');
 $subfacts=$olap->getFacts($cube,$facts,$filter);
 
 echo '<pre>'.print_r($data,true).'</pre>';
-die;
-
-
-//$olap->createCube($fact); // will auto detect
-
-//$olap->aggregate($cube)
-
-
-
-//data pattern as this:
-/*
-	$fact[$fieldname]=[$dimensionvalue:{...}..];
-	$fact['country']=[MY:[{},{},{}],SG:[],US:[]...];
-
-	$cube = [
-		dimension:dimensions,
-		measures:measures,
-		facts:facts,
-		dimensionlist:['county':[MY,SG,]]
-		sufacts:[
-			'country':{MY:[1,4],'SG':[2],'US':[3]},
-			'agent':{A:[1,4],'B':[2],'C':[3]},
-		],
-		aggregate: 
-			[
-			'country':[
-				subset: {MY:[1,4],'SG':[2],'US':[3]},
-				'sum': {'MY':3455,'SG':123,'US':2},
-				'AVG': {'MY':23,'SG':3,'US':1},
-				],
-			'country':[
-				subset: {MY:[1,4],'SG':[2],'US':[3]},
-				'sum': {'MY':3455,'SG':123,'US':2},
-				'AVG': {'MY':23,'SG':3,'US':1},
-				],
-
-			]
-	]
-*/
